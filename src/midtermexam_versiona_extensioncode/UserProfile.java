fce01102734c3f283d7c7e788fbfda655e1b1736
RehmatpalKaur <157609775+RehmatpalKaur@users.noreply.github.com>
2024-02-21 10:45:44 -0500

    Update UserProfile.java
    comment added

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -11,7 +11,8 @@ package midtermexam_versiona_extensioncode;
  * genres array. This code is to be used for SYST 17796 midterm exam.
  * Students are reminded to add themselves as modifiers when editing the
  * code.
- * @author dancye, 2019
+ * @author Rehmat
+ *This comment we will be using to fetch
  */
 public class UserProfile 
 {
